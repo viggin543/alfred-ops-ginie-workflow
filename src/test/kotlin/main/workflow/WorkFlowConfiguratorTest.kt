59be097c4b8a7db4e77eecbdb0c7f9df967fda34
Domrev Igor <domrevigor@gmail.com>
2019-04-05 21:37:02 +0300

    adding config assertion and acking

--- src/test/kotlin/main/workflow/WorkFlowConfiguratorTest.kt
+++ src/test/kotlin/main/workflow/WorkFlowConfiguratorTest.kt
@@ -1,27 +1,18 @@
 package main.workflow
 
 import com.nhaarman.mockitokotlin2.mock
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.Test
 import kotlin.test.assertFalse
 
 internal class WorkFlowConfiguratorTest {
 
-    companion object {
-        @JvmStatic
-        fun argLists() = listOf(
-            Arguments.of(listOf<String>()),
-            Arguments.of(listOf("banana"))
-        )
-    }
 
-    @ParameterizedTest
-    @MethodSource("argLists")
-    fun `isConfigureCommand false case`(args: List<String>) {
+    @Test
+    fun `isConfigureCommand false case`() {
         val workflow: Workflow = mock()
         val unit = WorkFlowConfigurator(workflow)
-        assertFalse(unit.isConfigureCommand(args))
+        assertFalse(unit.isConfigureCommand(listOf()))
+        assertFalse(unit.isConfigureCommand(listOf("banana")))
     }
 
 }
